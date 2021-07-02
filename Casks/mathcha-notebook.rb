f61a17b8dedc13126585262bb6dd8c59a39daaa7
Timo Früh <timo.frueh@icloud.com>
2021-07-03 00:03:19 +0200

    change URL of mathcha-notebook to a zip download

--- Casks/mathcha-notebook.rb
+++ Casks/mathcha-notebook.rb
@@ -2,7 +2,7 @@ cask "mathcha-notebook" do
   version "latest"
   sha256 :no_check
 
-  url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-#{version}.dm"
+  url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-#{version}-mac.zip"
   name "mathcha-notebook"
   desc "An easy editor for math"
   homepage "https://www.mathcha.io/"
