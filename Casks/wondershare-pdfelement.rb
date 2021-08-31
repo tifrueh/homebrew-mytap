3cffb2132a355ab4a8371b9b2d572694ea6d415e
Timo Früh <timo.frueh@icloud.com>
2021-08-31 17:27:54 +0200

    add wondershare-pdfelement, version 8

---
+++ Casks/wondershare-pdfelement.rb
@@ -0,0 +1,11 @@
+cask "wondershare-pdfelement" do
+  version "8.5.0"
+  sha256 :no_check
+
+  url "https://download.wondershare.com/mac-pdfelement_full5237.dmg"
+  name "wondershare-pdfelement"
+  desc "Wondershare's PDF editor"
+  homepage "https://pdf.wondershare.com"
+
+  app "Wondershare PDFelement"
+end
