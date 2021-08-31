0900056ee1adceebc091c44734ee0487ec21db31
Timo Früh <timo.frueh@icloud.com>
2021-08-31 18:23:33 +0200

    update wondershare-pdfelement

--- Casks/wondershare-pdfelement.rb
+++ Casks/wondershare-pdfelement.rb
@@ -1,5 +1,5 @@
 cask "wondershare-pdfelement" do
-  version "8.5.0"
+  version :latest
   sha256 :no_check
 
   url "https://download.wondershare.com/mac-pdfelement_full5237.dmg"
@@ -7,5 +7,22 @@ cask "wondershare-pdfelement" do
   desc "Wondershare's PDF editor"
   homepage "https://pdf.wondershare.com"
 
-  app "Wondershare PDFelement"
+  installer script: {
+    executable: "Wondershare PDFelement Installer.app/Contents/MacOS/Product Installer"
+  }
+
+  uninstall quit: [
+    "com.wondershare.PDFelement",
+    "com.wondershare.helper_compact"
+     ],
+
+    delete: [
+      "~/Library/Application Support/com.wondershare.PDFelement",
+      "~/Library/Application Support/com.wondershare.Installer",
+      "~/Library/Caches/PDFelement",
+      "~/Library/Caches/com.wondershare.PDFelement",
+      "~/Library/Saved Application State/com.wondershare.PDFelement.savedState",
+      "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.wondershare.PDFelement",
+      "/Applications/PDFelement.app",
+    ]
 end
