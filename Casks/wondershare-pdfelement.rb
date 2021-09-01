4d2bb4c085ec68d6b19394f0d9cfd15a8f3da46f
Timo Früh <timo.frueh@icloud.com>
2021-09-01 17:52:07 +0200

    style fix wondershare-pdfelement

--- Casks/wondershare-pdfelement.rb
+++ Casks/wondershare-pdfelement.rb
@@ -5,25 +5,24 @@ cask "wondershare-pdfelement" do
   url "https://download.wondershare.com/cbs_down/mac-pdfelement_full5237.zip"
   name "Wondershare PDFelement for Mac"
   desc "Create, edit, convert and sign PDF documents"
-  homepage "https://pdf.wondershare.com"
+  homepage "https://pdf.wondershare.com/"
 
   depends_on macos: ">= :sierra"
 
   app "PDFelement.app"
 
-  uninstall quit: [
+  uninstall quit:   [
     "com.wondershare.PDFelement",
-    "com.wondershare.helper_compact"
-     ],
+    "com.wondershare.helper_compact",
+  ],
 
-    delete: [
-      "~/Library/Application Support/com.wondershare.PDFelement",
-      "~/Library/Application Support/com.wondershare.Installer",
-      "~/Library/Caches/PDFelement",
-      "~/Library/Caches/com.wondershare.PDFelement",
-      "~/Library/Saved Application State/com.wondershare.PDFelement.savedState",
-      "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.wondershare.PDFelement",
-      "/Applications/PDFelement.app",
-    ]
-
+            delete: [
+              "~/Library/Application Support/com.wondershare.PDFelement",
+              "~/Library/Application Support/com.wondershare.Installer",
+              "~/Library/Caches/PDFelement",
+              "~/Library/Caches/com.wondershare.PDFelement",
+              "~/Library/Saved Application State/com.wondershare.PDFelement.savedState",
+              "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.wondershare.PDFelement",
+              "/Applications/PDFelement.app",
+            ]
 end
