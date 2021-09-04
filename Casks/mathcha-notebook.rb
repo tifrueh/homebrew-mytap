10076361f67e21416510c181c74a6e31d6435167
Timo Früh <timo.frueh@icloud.com>
2021-09-04 18:31:42 +0200

    update mathcha

--- Casks/mathcha-notebook.rb
+++ Casks/mathcha-notebook.rb
@@ -1,10 +1,10 @@
 cask "mathcha-notebook" do
   version :latest
-  sha256 :no_check
+  sha256 "b694b1c6343466b39ce4692ba8be53681557662cac1758dc876b079c76fb6773"
 
   url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-#{version}-mac.zip"
   name "mathcha-notebook"
-  desc "Easy editor for math"
+  desc "Mathematics Editor"
   homepage "https://www.mathcha.io/"
 
   app "Mathcha Notebook.app"
