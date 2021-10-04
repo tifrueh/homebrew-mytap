fb7d19ebc76f7a74b9169e73c5914c841ec14332
Timo Früh <timo.frueh@icloud.com>
2021-10-05 01:30:27 +0200

    fix shasum

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,6 +1,6 @@
 cask "mlaatkst" do
   version "1.0.0"
-  sha256 "045b04c76482ecac23cf7bf47934b62f3a612b5ace7556b7ac563808e848e9e4"
+  sha256 "9c1e98dd855feb5881772487ce0f1c2a863417b28265cabf001bb183de38693c"
 
   url "https://github.com/Timo-Frueh/mlaatkst/releases/download/v#{version}/MLAatKST.dmg"
   name "MLAatKST"
