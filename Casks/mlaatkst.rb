11ae661a8845e32eac3a6d27214a246fd751ed43
Timo Früh <timo.frueh@icloud.com>
2021-10-05 10:02:51 +0200

    fix problem with mlaatkst

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,8 +1,8 @@
 cask "mlaatkst" do
   version "1.0.0"
-  sha256 "9c1e98dd855feb5881772487ce0f1c2a863417b28265cabf001bb183de38693c"
+  sha256 "f3b049cfe0adcf7b99ee8a852f9c837ac0f2fe2371a81753564fd9223aa6b95b"
 
-  url "https://github.com/Timo-Frueh/mlaatkst/releases/download/v#{version}/MLAatKST.dmg"
+  url "https://github.com/Timo-Frueh/mlaatkst/releases/download/v#{version}/MLAatKST.zip"
   name "MLAatKST"
   desc "Citation helper for KST students"
   homepage "https://github.com/Timo-Frueh/mlaatkst"
