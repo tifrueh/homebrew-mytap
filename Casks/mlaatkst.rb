338a95a267e924510446315b00569e9571f90050
Timo Früh <timo.frueh@icloud.com>
2021-10-05 23:08:24 +0200

    changes to mlaatkst

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,6 +1,6 @@
 cask "mlaatkst" do
-  version "1.0.0"
-  sha256 "f3b049cfe0adcf7b99ee8a852f9c837ac0f2fe2371a81753564fd9223aa6b95b"
+  version "1.1.0"
+  sha256 "e4944df3566c728e4a806a3912aa50200180679902e72752074d4506ea529726"
 
   url "https://github.com/Timo-Frueh/mlaatkst/releases/download/v#{version}/MLAatKST.zip"
   name "MLAatKST"
