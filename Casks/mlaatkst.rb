4ab6a7b3b24c4e285476fc3d4853e07aa2ac9cf3
Timo Früh <timo.frueh@icloud.com>
2022-02-19 23:06:36 +0100

    update mlaatkst

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,8 +1,8 @@
 cask "mlaatkst" do
-  version "1.2.2"
-  sha256 "bea05f8abdfbce65f63fb4479c32520116bf537d5a3eb57936948a69bbe56faa"
+  version "1.2.3"
+  sha256 "ab58c7e65140a1cebd75d5758f223ddf3b849fce4b1efb8a0bc2c6a920e4f68b"
 
-  url "https://github.com/Timo-Frueh/mlaatkst/releases/download/v#{version}/MLAatKST-macOS-universal.zip"
+  url "https://github.com/tifrueh/mlaatkst/releases/download/v#{version}/MLAatKST-macOS-universal-v#{version}.zip"
   name "mlaatkst"
   desc "Citation helper for KST students"
   homepage "https://github.com/Timo-Frueh/mlaatkst"
