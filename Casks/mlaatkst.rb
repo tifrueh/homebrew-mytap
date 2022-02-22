fdf641ef689fc371fb86049697d614341e90c8fe
Timo Früh <timo.frueh@icloud.com>
2022-02-22 08:48:25 +0100

    update mlaatkst

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,6 +1,6 @@
 cask "mlaatkst" do
-  version "1.2.3"
-  sha256 "ab58c7e65140a1cebd75d5758f223ddf3b849fce4b1efb8a0bc2c6a920e4f68b"
+  version "1.2.4"
+  sha256 "a1c2b417fe7f6b93a757ddc2b4c19562af17478131d414697b6cb1c206954e72"
 
   url "https://github.com/tifrueh/mlaatkst/releases/download/v#{version}/MLAatKST-macOS-universal-v#{version}.zip"
   name "mlaatkst"
