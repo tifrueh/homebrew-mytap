18e81b35ab1bb6e3337269c9a96242151bfc521f
Timo Früh <timo.frueh@icloud.com>
2022-04-13 12:32:49 +0200

    update mlaatkst to use a PKG installer

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,13 +1,15 @@
 cask "mlaatkst" do
   version "1.2.4"
-  sha256 "a1c2b417fe7f6b93a757ddc2b4c19562af17478131d414697b6cb1c206954e72"
+  sha256 "b2ff79904d81b5717c3ba895ff2270e1879767dd055087c6fb3ac09a5141c12f"
 
-  url "https://github.com/tifrueh/mlaatkst/releases/download/v#{version}/MLAatKST-macOS-universal-v#{version}.zip"
+  url "https://github.com/tifrueh/mlaatkst/releases/download/v#{version}/MLAatKST-macOS-universal-v#{version}.pkg"
   name "mlaatkst"
   desc "Citation helper for KST students"
   homepage "https://github.com/Timo-Frueh/mlaatkst"
 
-  app "MLAatKST.app"
+  pkg "MLAatKST-macOS-universal-v#{version}.pkg"
+
+  uninstall pkgutil: "com.tifrueh.mlaatkst"
 
   zap trash: [
     "~/Library/Preferences/com.tifrueh.mlaatkst.plist",
