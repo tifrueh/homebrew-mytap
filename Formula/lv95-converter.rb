10d6e27d7e7cfc77a3b9261f1e8fa481e2d56bd6
Timo Früh <timo.frueh@icloud.com>
2023-01-07 22:35:13 +0100

    update lv95-converter

--- Formula/lv95-converter.rb
+++ Formula/lv95-converter.rb
@@ -1,17 +1,19 @@
 class Lv95Converter < Formula
   desc "Convert LV95 to WGS84 and vice versa"
   homepage "https://github.com/tifrueh/lv95-converter"
-  url "https://github.com/tifrueh/lv95-converter/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "8cf6bdd41af84e7fe82a52543d7c663d7c46b62e7e7b6ea89644141663b48528"
+  url "https://github.com/tifrueh/lv95-converter/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "96924ad3f1c8a48a9afacf4ffad97a7b8bd06b3c740d41f6e3e556254df5f3bc"
   license "GPL-3.0-or-later"
 
-  depends_on "cli11"
+  depends_on "cli11" :build
 
   def install
     system "make"
+    system "gzip man/lv95-converter.1"
     prefix.install "LICENSE.md"
     prefix.install "README.md"
     bin.install "build/lv95-converter"
+    man1.install "man/lv95-converter.1.gz"
   end
 
   test do
