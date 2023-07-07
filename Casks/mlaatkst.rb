6f83c2b4a4d2dd96d078400a1e8f7ffab6195625
Timo Früh <timo.frueh@icloud.com>
2023-07-08 00:08:40 +0200

    update mlaatkst

--- Casks/mlaatkst.rb
+++ Casks/mlaatkst.rb
@@ -1,13 +1,13 @@
 cask "mlaatkst" do
-  version "2.1.1"
-  sha256 "0a58d7ecbff15a3c9f9de4b9ad25c3f5558f0ba943e8c3ce749e36b1572c2a58"
+  version "2.1.2"
+  sha256 "fac4531bf47e77c1391d34bee7f959b8379e50f2e3341f591357f2d2b55ebbe4"
 
   url "https://github.com/tifrueh/mlaatkst/releases/download/v#{version}/MLAatKST-macOS-universal-v#{version}.pkg"
   name "mlaatkst"
   desc "Footnote helper for KST students"
   homepage "https://github.com/tifrueh/mlaatkst"
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">=11"
 
   pkg "MLAatKST-macOS-universal-v#{version}.pkg"
 
