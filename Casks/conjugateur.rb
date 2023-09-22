1c45e965a2db5adce27d7e002a1ababf47991da4
Timo Früh <timo.frueh@icloud.com>
2023-09-22 17:39:01 +0200

    update conjugateur

--- Casks/conjugateur.rb
+++ Casks/conjugateur.rb
@@ -1,6 +1,6 @@
 cask "conjugateur" do
-  version "1.0.0-alpha"
-  sha256 "f840b4d38ee692143ac88176c72ed3d541f765d08aa81cdf2720be406c1eb967"
+  version "1.0.0-alpha-2"
+  sha256 "327bf29eab78fe814698e3d42d736fc9f200dd4bdd52fef0fb3d31980e9946da"
 
   url "https://github.com/tifrueh/conjugateur/releases/download/v#{version}/Conjugateur-macOS-universal-v#{version}.pkg"
   name "conjugateur"
