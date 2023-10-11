cf98919fed539994b8fc2e99d74e796f911bc144
Timo Früh <timo.frueh@icloud.com>
2023-10-11 19:12:33 +0200

    update conjugateur

--- Casks/conjugateur.rb
+++ Casks/conjugateur.rb
@@ -1,6 +1,6 @@
 cask "conjugateur" do
-  version "1.0.0-alpha-3"
-  sha256 "788adf91cddcf2b87c9535b0ecb9b10245287f2917ee564b6229dec892701739"
+  version "1.0.0-alpha-4"
+  sha256 "ec1d291b8c17d20877c51a1fb3981a3dbc55fcaf200ae11e233cbee4fdbb0485"
 
   url "https://github.com/tifrueh/conjugateur/releases/download/v#{version}/Conjugateur-macOS-universal-v#{version}.pkg"
   name "conjugateur"
@@ -11,11 +11,11 @@ cask "conjugateur" do
 
   pkg "Conjugateur-macOS-universal-v#{version}.pkg"
 
-  uninstall pkgutil: "com.tifrueh.conjugateur"
+  uninstall pkgutil: "ch.tifrueh.conjugateur"
 
   zap trash: [
-    "~/Library/Preferences/com.tifrueh.conjugateur.plist",
+    "~/Library/Preferences/ch.tifrueh.conjugateur.plist",
     "~/Library/Preferences/conjugateur.plist",
-    "~/Library/Saved Application State/com.tifrueh.conjugateur.savedState",
+    "~/Library/Saved Application State/ch.tifrueh.conjugateur.savedState",
   ]
 end
