ee7d21ccc7df05494a3c1899504a1a4d29c25063
Timo Früh <timo.frueh@icloud.com>
2023-10-14 15:15:49 +0200

    update conjugateur

--- Casks/conjugateur.rb
+++ Casks/conjugateur.rb
@@ -1,15 +1,23 @@
 cask "conjugateur" do
   version "1.0.0-beta"
-  sha256 "399fdd6681bf0919724abc7c57004968b082036515f90d4a49cae70d3dcfcf09"
+  
+  depends_on macos: ">= :ventura"
 
-  url "https://github.com/tifrueh/conjugateur/releases/download/v#{version}/Conjugateur-macOS-Sonoma-universal-v#{version}.pkg"
+  on_sonoma do
+    sha256 "399fdd6681bf0919724abc7c57004968b082036515f90d4a49cae70d3dcfcf09"
+    url "https://github.com/tifrueh/conjugateur/releases/download/v#{version}/Conjugateur-macOS-Sonoma-universal-v#{version}.pkg"
+    pkg "Conjugateur-macOS-Sonoma-universal-v#{version}.pkg"
+  end
+
+  on_ventura do
+    sha256 "9f0d233289d71d29a1225674baaaff5dce06e708d69f99d8128aba4be937ac60"
+    url "https://github.com/tifrueh/conjugateur/releases/download/v#{version}/Conjugateur-macOS-Ventura-universal-v#{version}.pkg"
+    pkg "Conjugateur-macOS-Ventura-universal-v#{version}.pkg"
+  end
+
   name "conjugateur"
   desc "French verb conjugation trainer"
   homepage "https://github.com/tifrueh/conjugateur"
-
-  depends_on macos: ">= :sonoma"
-
-  pkg "Conjugateur-macOS-Sonoma-universal-v#{version}.pkg"
 
   uninstall pkgutil: "ch.tifrueh.conjugateur"
 
