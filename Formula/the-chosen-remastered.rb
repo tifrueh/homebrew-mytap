1664bfeadd0556fdc9738faa0770276a9627ecd2
Timo Früh <timo.frueh@icloud.com>
2024-01-16 15:41:12 +0100

    Update the-chosen-remastered

--- Formula/the-chosen-remastered.rb
+++ Formula/the-chosen-remastered.rb
@@ -1,12 +1,11 @@
 class TheChosenRemastered < Formula
   desc "Short ZORK-like text adventure"
   homepage "https://github.com/tifrueh/the-chosen-remastered"
-  url "https://github.com/tifrueh/the-chosen-remastered/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "8d095929a7b0b489e1741b581750384e0599687a7170b8c069bfef0ade8223ef"
+  url "https://github.com/tifrueh/the-chosen-remastered/releases/download/v1.0.2/the-chosen-remastered-1.0.2.tar.xz"
+  sha256 "25ba4529eb981bd94f46279a540dc57151ebc126c9118f41510f101311dc4ac0"
   license "GPL-3.0-or-later"
 
   depends_on "meson" => :build
-  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
 
   uses_from_macos "ncurses"
@@ -18,6 +17,6 @@ class TheChosenRemastered < Formula
   end
 
   test do
-    assert_equal "1.0.1", shell_output("#{bin}/the-chosen-remastered -v")
+    assert_equal "v1.0.2", shell_output("#{bin}/the-chosen-remastered -v")
   end
 end
