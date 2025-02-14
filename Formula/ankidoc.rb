c906c5671784e6b96aa65e7aab0b34252864ab53
Timo Früh <dev@tifrueh.ch>
2025-02-14 21:27:04 +0100

    Update ankidoc

--- Formula/ankidoc.rb
+++ Formula/ankidoc.rb
@@ -3,8 +3,8 @@ class Ankidoc < Formula
 
   desc "Convert asciidoc notes to anki notes"
   homepage "https://github.com/tifrueh/ankidoc/"
-  url "https://github.com/tifrueh/ankidoc/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "42f6ca414239b3191834e569033251d314afa518a866a44d072e6e1174bfc66d"
+  url "https://github.com/tifrueh/ankidoc/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9aeb98d91720222d7d05cb7d43b0effa6504f700c61a242dd5bb6a9c45492bff"
   license "GPL-3.0-or-later"
 
   depends_on "python"
