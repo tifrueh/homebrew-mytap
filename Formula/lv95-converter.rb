4e921d319f70728c189911a093d9e133f51901f7
Timo Früh <dev@tifrueh.ch>
2025-12-16 16:04:35 +0100

    Update lv95-converter

--- Formula/lv95-converter.rb
+++ Formula/lv95-converter.rb
@@ -1,8 +1,9 @@
 class Lv95Converter < Formula
   desc "Convert LV95 to WGS84 and vice versa"
   homepage "https://github.com/tifrueh/lv95-converter"
-  url "https://github.com/tifrueh/lv95-converter/releases/download/v1.1.2/lv95-converter-1.1.2.tar.xz"
-  sha256 "3b30e3201be2305ca378871ae148a6b457dbb73db8ebf24d6ee7cf058293a9b6"
+  url "https://github.com/tifrueh/lv95-converter/archive/refs/tags/v1.2.0.tar.gz"
+  head "https://github.com/tifrueh/lv95-converter.git", branch: "development"
+  sha256 "5b16bdbafe0bec352c4cd0520853d83184d34d917611e5f4e7b735c09d92711e"
   license "GPL-3.0-or-later"
 
   depends_on "meson" => :build
@@ -17,9 +18,7 @@ class Lv95Converter < Formula
   end
 
   test do
-    system("lv95-converter")
-    assert_equal "Coordinates in WGS84: N 46.9511, E 7.43864",
-shell_output("lv95-converter lv95 --east 2600000 --north 1200000").strip
-    assert_equal "v1.1.2", shell_output("lv95-converter --version")
+    system "lv95-converter --help"
+    assert_equal "Coordinates in WGS84: E 7.438637 N 46.951081", -shell_output("lv95-converter lv95 --east 2600000 --north 1200000").strip
   end
 end
